e5405f37cb3817173205c40b3cf7884dfdb67665
bete-de <ace21diamonds>
2023-02-17 11:11:43 -0800

    hasBEGUN

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
-#include <studio.h>
+#include <stdio.h>
 /**
- * main - A program that prints a line with puts function
- * Return: 0 (Success)
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
  */
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
-}
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+
+}	
