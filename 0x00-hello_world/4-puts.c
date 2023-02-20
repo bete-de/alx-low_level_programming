7b029a2822c3c98154e71786af3120c92cd90e58
bete-de <ace21diamonds>
2023-02-20 12:25:58 -0800

    forward

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+
 /**
-*main - entry point
+*main - this is the main function
+*
 *return: always 0 (success)
 */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
