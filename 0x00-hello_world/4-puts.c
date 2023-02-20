6f72fb113aa966b806708f1e99f89396c4b69302
bete-de <ace21diamonds>
2023-02-20 01:36:28 -0800

    FORWARD

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * Return: Always 0 (success)
- */
- int main(void)
- {
-     puts("\"Programming is like building a mulilingual puzzle");
-     return (0);
- }
+*main - entry point
+*return: always 0 (success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
