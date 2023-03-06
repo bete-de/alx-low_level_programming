40f00769374af8d263379b75b4ba5a7a5516f362
bete-de <ace21diamonds>
2023-03-06 10:45:10 -0800

    advance 1/2

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: content is to be copied
+ *@to: string
+ *Return: void
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+
